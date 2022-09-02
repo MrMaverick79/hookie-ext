47780d9f0721d7b8f31d1b4c2c1c9d190acd0c93
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-09-02 16:43:25 +1000

    changes post presentation

---
+++ popup/hookie.js
@@ -0,0 +1,13 @@
+
+
+function grabUrl() {
+    
+    document.body.style.border = "5px solid red"
+    
+}
+
+// document.body.style.border = "5px solid red";
+
+document.getElementById("url").value = window.location.href
+
+document.getElementById("name").value = tabs.activeTab.url
